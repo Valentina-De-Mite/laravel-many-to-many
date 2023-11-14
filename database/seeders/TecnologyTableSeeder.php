aae981c68dc15df35dbdc432b43141406269dff7
Valentina De Mite <valesakura97@gmail.com>
2023-11-14 15:01:43 +0100

    added tecnology table and seeder

---
+++ database/seeders/TecnologyTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tecnology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TecnologyTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tecnologies = ['php', 'laravel', 'programming', 'js', 'css', 'mysql', 'debugging'];
+
+        foreach($tecnologies as $tecnology){
+            $new_tecnology = new Tecnology();
+        
+            $new_tecnology->name = $tecnology;
+            
+            $new_tecnology->save();
+        }
+    }
+}
