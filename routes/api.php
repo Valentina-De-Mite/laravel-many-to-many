78cbae1847b3c236c2656ab264e0f709a3a50ef8
Valentina De Mite <valesakura97@gmail.com>
2023-11-17 14:59:35 +0100

    added end-poin with api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Project;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +20,34 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+// Route::get('projects', function() {
+//     return [
+//         'status' => 'success',
+//         'result' => 'Ciao Apina'
+//     ];
+// });
+
+Route::get('projects', function() {
+    return response()->json([
+    'status' => 'success',
+    'result' => Project::all()
+    ]);
+    
+});
+
+Route::get('categories', function() {
+    return response()->json([
+    'status' => 'success',
+    'result' => App\Models\Category::all()
+    ]);
+    
+});
+
+Route::get('tecnologies', function() {
+    return response()->json([
+    'status' => 'success',
+    'result' => App\Models\Tecnology::all()
+    ]);
+    
+});
